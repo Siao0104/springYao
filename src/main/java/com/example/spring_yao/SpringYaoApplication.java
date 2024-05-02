cdc49acaf2a1b8c9ba4ade03feed855fabb0c895
shiYao <shiny7723@gmail.com>
2024-05-02 15:54:57 +0800

    [ADD]schedule排程 & quartz配置檔

--- src/main/java/com/example/spring_yao/SpringYaoApplication.java
+++ src/main/java/com/example/spring_yao/SpringYaoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class SpringYaoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/spring_yao/utils/ScheduleDaily.java
@@ -0,0 +1,18 @@
+package com.example.spring_yao.utils;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduleDaily {
+
+    private static final SimpleDateFormat sdfHms = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(cron = "0 * 0/1 * * ?")
+    private void timingJob(){
+        System.out.println(STR."現在時間 : \{sdfHms.format(new Date())}");
+    }
+}
