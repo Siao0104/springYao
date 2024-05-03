2e216f12cce3b9458dfddab13da4ccdd1966154a
shiYao <shiny7723@gmail.com>
2024-05-03 11:44:02 +0800

    [FIX]變更MQ目錄

---
+++ src/main/java/com/example/spring_yao/utils/rabbitMq/RabbitmqRegister.java
@@ -0,0 +1,47 @@
+package com.example.spring_yao.utils.rabbitMq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqRegister {
+
+    @Bean
+    public Queue yao01A() { return new Queue(RabbitmqConstant.YAO_01_A); }
+
+    @Bean
+    public Queue yao01B() { return new Queue(RabbitmqConstant.YAO_01_B); }
+
+    @Bean
+    public Queue yao01C() { return new Queue(RabbitmqConstant.YAO_01_C); }
+
+    @Bean
+    public FanoutExchange yao01Exchange(){
+        return new FanoutExchange(RabbitmqConstant.YAO_01_EXCHANGE);
+    }
+
+    @Bean
+    public Binding yao01AFanoutExchange(){
+        return BindingBuilder.bind(yao01A()).to(yao01Exchange());
+    }
+
+    @Bean
+    public Binding yao01BFanoutExchange(){
+        return BindingBuilder.bind(yao01B()).to(yao01Exchange());
+    }
+
+    @Bean
+    public Binding yao01CFanoutExchange(){
+        return BindingBuilder.bind(yao01C()).to(yao01Exchange());
+    }
+
+    //Direct範例
+    @Bean
+    public Queue Queue() {
+        return new Queue("DirectMq");
+    }
+}
