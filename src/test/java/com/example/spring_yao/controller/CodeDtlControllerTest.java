423dc1855def5172e14d3966269b46e1aa0ecb06
shiYao <shiny7723@gmail.com>
2024-05-07 08:42:21 +0800

    [IMP]單元測試調整(未完成版)

--- src/test/java/com/example/spring_yao/controller/CodeDtlControllerTest.java
+++ src/test/java/com/example/spring_yao/controller/CodeDtlControllerTest.java
@@ -2,18 +2,15 @@ package com.example.spring_yao.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class CodeDtlControllerTest {
@@ -30,14 +27,6 @@ class CodeDtlControllerTest {
 
     @Test
     void uiGetDtlCodeDesc() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/springYao/codeDtl/uiGetDtlCodeDesc/JP")
-                        .accept(MediaType.ALL)
-                        .session(session))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(MockMvcResultHandlers.print())
-                .andReturn();
-        int status = mvcResult.getResponse().getStatus();
-        String content = mvcResult.getResponse().getContentAsString();
-        System.out.println(STR."content : \{content},status : \{status}");
+
     }
 }
